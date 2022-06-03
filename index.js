65019fe08d149ba701fe9e7e64f78a1484b18837
Narek <narektom333@gmail.com>
2022-06-03 20:07:34 +0400

    added node modules, package.json, index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+const fs = require("fs");
+const http = require("http");
+const path = require("path");
+
+const extType = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript"
+}
+console.log("hiiii");
+
+function filePathFunc(req, res, next) {
+    let url = req.url;
+    if (url === "/") {
+        url = "/index.html";
+    }
+    const filePath = path.resolve("public" + url)
+    fs.promises.access(filePath)
+        .then(() => {
+            const ext = path.extname(filePath);
+            res.writeHead(200, {
+                'Content-type': extType[ext]
+            });
+            fs.createReadStream(filePath).pipe(res)
+        })
+        .catch(() => {
+            next();
+        })
+}
+
+const server = http.createServer((req, res) => {
+    filePathFunc(req, res, () => {
+        if (req.url === "/hello") {
+            res.writeHead(200, {
+                'Content-type': 'text/plain'
+            });
+            res.end('barev dzez Narek!!!!')
+        } else if (req.url === "/bye") {
+            res.writeHead(200, {
+                "Content-Type": "application/json"
+            });
+            res.end(JSON.stringify({
+                name: "Narek"
+            }))
+        } else {
+            res.writeHead(404, {
+                "Content-type": "text/plain"
+            });
+            res.end("Not found");
+        }
+    })
+
+})
+
+server.listen(5000)
